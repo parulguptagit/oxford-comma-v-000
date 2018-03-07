2ef975d8bbb61b47b1cc2859ea2250835b5bab02
Parul Gupta <parulguptasmail@gmail.com>
2018-03-07 20:23:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
   if array.size > 2
     array[-1] = "and #{array[-1]}"
+    array.join(", ")
   elsif array.size == 2 
      array.join(" and ")
-  
- array.join(", ")
+  else
+     array
+  end
   
 end
